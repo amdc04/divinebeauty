8e3d2916bdc34f6a34e13ca419a3dbb68963b34a
amdc04 <bino.dlcruz@gmail.com>
2021-06-03 12:28:45 +1200

    add to pay

---
+++ topay.php
@@ -0,0 +1,16 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="4KXH5SH9GX2LA">
+<table>
+<tr><td><input type="hidden" name="on0" value="Services">Services</td></tr><tr><td><select name="os0">
+	<option value="Polish Only">Polish Only $17.00 NZD</option>
+	<option value="Shape & Polish">Shape & Polish $25.00 NZD</option>
+	<option value="Manicure or Pedicure">Manicure or Pedicure $30.00 NZD</option>
+  <option value="MAN-icure">MAN-icure $45.00 NZD</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="NZD">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+ 
